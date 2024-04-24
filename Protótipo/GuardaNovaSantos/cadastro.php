dd2f9f2efc78896f0126904de659c1a36fac3f64
GuilhermeBotingnon <guilherme.botingnon@gmail.com>
2024-04-24 13:10:33 -0300

    update cadastr

--- Protótipo/GuardaNovaSantos/cadastro.php
+++ Protótipo/GuardaNovaSantos/cadastro.php
@@ -6,81 +6,16 @@
     <title>Gns - Cadastro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css\stylecadastros.css"> 
-    <link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon"> 
+    <link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon">
+    <script src="js/script.js"></script>
  
     <style>
   .txtsucesso{
  color: yellow;
- font-size: 2em;
+ font-size: 1em;
  font-weight: 450;
   }
 </style>
-<script>
-    
-    function limpa_formulário_cep() {
-            //Limpa valores do formulário de cep.
-            document.getElementById('bairro').value=("");
-            document.getElementById('cidade').value=("");
-            document.getElementById('rua').value=("");
-    }
-
-    function meu_callback(conteudo) {
-        if (!("erro" in conteudo)) {
-            //Atualiza os campos com os valores.
-            document.getElementById('rua').value=(conteudo.logradouro);
-            document.getElementById('bairro').value=(conteudo.bairro);
-            document.getElementById('cidade').value=(conteudo.localidade);
-
-        } //end if.
-        else {
-            //CEP não Encontrado.
-            limpa_formulário_cep();
-            alert("CEP não encontrado.");
-        }
-    }
-        
-    function pesquisacep(valor) {
-
-        //Nova variável "cep" somente com dígitos.
-        var cep = valor.replace(/\D/g, '');
-
-        //Verifica se campo cep possui valor informado.
-        if (cep != "") {
-
-            //Expressão regular para validar o CEP.
-            var validacep = /^[0-9]{8}$/;
-
-            //Valida o formato do CEP.
-            if(validacep.test(cep)) {
-
-                //Preenche os campos com "..." enquanto consulta webservice.
-                document.getElementById('bairro').value="...";
-                document.getElementById('cidade').value="...";
-                document.getElementById('rua').value="...";
-
-                //Cria um elemento javascript.
-                var script = document.createElement('script');
-
-                //Sincroniza com o callback.
-                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
-
-                //Insere script no documento e carrega o conteúdo.
-                document.body.appendChild(script);
-
-            } //end if.
-            else {
-                //cep é inválido.
-                limpa_formulário_cep();
-                alert("Formato de CEP inválido.");
-            }
-        } //end if.
-        else {
-            //cep sem valor, limpa formulário.
-            limpa_formulário_cep();
-        }
-    };
-
-    </script>
   <?php
 $txt ='';
 
@@ -108,7 +43,7 @@ $_POST['profissao'],
 $_POST['experiencia']
   );
  
-  include_once("conexaocooperado.php");
+  include_once("conexao.php");
 
   try{
 
