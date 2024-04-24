9c24a2d30497a109f1ab99b72e2b8771e27e1ca7
GuilhermeBotingnon <guilherme.botingnon@gmail.com>
2024-04-24 11:10:11 -0300

    update

--- Protótipo/GuardaNovaSantos/login.php
+++ Protótipo/GuardaNovaSantos/login.php
@@ -1,27 +1,6 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Gns - Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous" async></script>
-    <link rel="stylesheet" href="/css/login.css">
-    <link rel="shortcut icon" href="/favicon/favicon.ico" type="image/x-icon">
-    <style>
-.error {
-    color:yellow;
-    font-size: 1em;
-    font-weight: 500;
-}
-</style>
 <?php
-
 include("conexaologin.php");
-
 if ($_POST){
-
      if (strlen($_POST['email'])== 0) {
       echo $error_email = "preencha seu email";
       } else if (strlen($_POST["senha"])== 0){
@@ -53,6 +32,24 @@ if ($_POST){
             }
      }
      ?>
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Gns - Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous" async></script>
+    <link rel="stylesheet" href="/css/login.css">
+    <link rel="shortcut icon" href="/favicon/favicon.ico" type="image/x-icon">
+    <style>
+.error {
+    color:yellow;
+    font-size: 1em;
+    font-weight: 500;
+}
+</style>
 </head>
 <body>
   <nav class="navbar fixed-top gnsCor">
@@ -65,7 +62,6 @@ if ($_POST){
         </a>
       </div>
   </nav>
-
 <main class="cor text-white fw-bold fs-2" style="font-family: Poppins;">
   <div class="container d-flex align-items-center justify-content-center vh-100">
     <div class="row">
