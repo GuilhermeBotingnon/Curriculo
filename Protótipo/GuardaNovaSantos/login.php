2afab9319674805b6f75ba26c92dfb3e4e43f63e
GuilhermeBotingnon <guilherme.botingnon@gmail.com>
2024-04-24 10:35:03 -0300

    update

--- Protótipo/GuardaNovaSantos/login.php
+++ Protótipo/GuardaNovaSantos/login.php
@@ -1,3 +1,21 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Gns - Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous" async></script>
+    <link rel="stylesheet" href="/css/login.css">
+    <link rel="shortcut icon" href="/favicon/favicon.ico" type="image/x-icon">
+    <style>
+.error {
+    color:yellow; /* Cor vermelha do Bootstrap */
+    font-size: 2em;
+    font-weight: 500;
+}
+</style>
 <?php
 
 include("conexaologin.php");
@@ -12,7 +30,7 @@ if ($_POST){
             $email = $mysqli-> real_escape_string($_POST["email"]);
             $senha = $mysqli->real_escape_string($_POST["senha"]);
             
-            $sql_code = "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
+            $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
             $sql_query = $mysqli->query($sql_code) or die("Falha na execução do codigo sql; " . $mysqli->error); 
              
            $quantidade = $sql_query->num_rows;
@@ -35,24 +53,6 @@ if ($_POST){
             }
      }
      ?>
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Gns - Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous" async></script>
-    <link rel="stylesheet" href="/css/login.css">
-    <link rel="shortcut icon" href="/favicon/favicon.ico" type="image/x-icon">
-    <style>
-.error {
-    color:yellow; /* Cor vermelha do Bootstrap */
-    font-size: 2em;
-    font-weight: 500;
-}
-</style>
 </head>
 <body>
   <nav class="navbar fixed-top gnsCor">
