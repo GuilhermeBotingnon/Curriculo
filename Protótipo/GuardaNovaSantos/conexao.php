1429bd35bc872341bbada6f3bf05b66e77ef5eca
GuilhermeBotingnon <guilherme.botingnon@gmail.com>
2024-04-25 13:39:38 -0300

    conexao php

--- Protótipo/GuardaNovaSantos/conexao.php
+++ Protótipo/GuardaNovaSantos/conexao.php
@@ -7,6 +7,13 @@ $host = 'roundhouse.proxy.rlwy.net:31956';
 
 $mysqli = new mysqli($host, $usuario , $senha , $database);
 
+try{
+    $conn = new PDO("mysql:host=$host;dbname=railway",$usuario,$senha);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+}
+
 if ($mysqli ->error){
     die("Erro ao Conectar com o DB :" . $mysqli ->error);
 } 
