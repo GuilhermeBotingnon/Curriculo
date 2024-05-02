b29fbd2c15e80a995a62c3440d87ef558d160ca7
GuilhermeBotingnon <guilherme.botingnon@gmail.com>
2024-05-01 21:51:31 -0300

    Atualização 2-

---
+++ Cursos/CursoReact/2_FUNDAMENTOS/fundamentos/src/components/MyComponent.js
@@ -0,0 +1,8 @@
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Estou sendo reaproveitado em vários lugares! </h1>
+    </div>
+  );
+};
+export default MyComponent;
---
+++ Cursos/CursoReact/2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.js
@@ -0,0 +1,17 @@
+const TemplateExpressions = () => {
+  const name = "Matheus";
+  const data = {
+    age: 31,
+    job: "Programmer",
+  };
+
+  return (
+    <div>
+      <h1> Olá {name}, Tudo Bem?</h1>
+      <p>Você Atua como: {data.job}</p>
+      <p>{4 + 4}</p>
+      <p>{console.log("JSX React")}</p>
+    </div>
+  );
+};
+export default TemplateExpressions;
