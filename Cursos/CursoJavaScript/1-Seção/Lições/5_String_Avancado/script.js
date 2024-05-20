b31524238c5766b335d2686a4229a4e79ffd2fc0
GuilhermeBotingnon <guilherme.botingnon@gmail.com>
2024-05-19 21:25:52 -0300

    19/05 Secão 1 Completa

---
+++ Cursos/CursoJavaScript/1-Seção/Lições/5_String_Avancado/script.js
@@ -0,0 +1,5 @@
+console.log(typeof "Meu nome é Guilherme");
+console.log(typeof "Vou me tornar um Desenvolvedor");
+console.log(typeof `Teste`);
+console.log(typeof Infinity);
+console.log(typeof "Infinity");
---
+++ Cursos/CursoJavaScript/1-Seção/Lições/8_Boolean_op_logico/script.js
@@ -0,0 +1,10 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+
+console.log(3 == 3 && "Guilherme" == "Guilherme");
+console.log("Guilherme" == "Pedro" || false);
+
+console.log(!!(true && true));
+
+console.log(true && true);
+console.log(false || false);
